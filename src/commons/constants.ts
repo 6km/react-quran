e03b76d8c3a5fc06d040a9a569470643c7a63a8b
Mohammed Taha <gsft22221@gmail.com>
2024-07-06 14:16:29 +0300

    feat(constants): add MEDINA_MUSHAF_CENTERED_LINES

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -10,3 +10,27 @@ export const SURAH_ATTAWBAH_ID = 9
 
 // Count of the pages of the Quran (Hafs narration)
 export const HAFS_PAGES_COUNT = 604
+
+/**
+ * Lines to center.
+ * There are some lines that should have verses in the center to match The Medina Mushaf (officially: Mushaf al-Madinah an-Nabawiyyah, Arabic: مصحف المدينة النبوية).
+ * For example, the last two verses of Surah Al-Qari'ah
+
+ * The structure of the data is an array of [SURAH_ID, PAGE_NUMBER, LINE_NUMBER_ON_PAGE]
+
+ * SURAH_ID: Used to ensure that the surah is in the provided LINE_NUMBER_ON_PAGE
+ * PAGE_NUMBER: Indicates the page where the line is located
+ * LINE_NUMBER_ON_PAGE: Specifies which line of the page should be centered
+ * 
+ * @constant [SURAH_ID, PAGE_NUMBER, LINE_NUMBER_ON_PAGE]
+ */
+export const MEDINA_MUSHAF_CENTERED_LINES = [
+    [13, 255, 2],
+    [53, 528, 9],
+    [55, 534, 6],
+    [58, 545, 6],
+    [80, 586, 1],
+    [88, 593, 2],
+    [89, 594, 5],
+    [101, 600, 8],
+]
