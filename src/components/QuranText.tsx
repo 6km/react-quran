bfc27667ce6e9d05a450f65c5e978a7da0a6e578
Mohammed Taha <gsft22221@gmail.com>
2024-07-24 19:15:23 +0300

    feat(QuranText): improve text rendering quality

--- src/components/QuranText.tsx
+++ src/components/QuranText.tsx
@@ -6,6 +6,10 @@ const Text = styled.span`
     font-style: normal;
     white-space: nowrap !important;
     text-transform: none !important;
+
+    font-smoothing: antialiased;
+    -webkit-font-smoothing: antialiased;
+    text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.004);
 `
 
 export default function QuranText({ text, className = '' }: { text: string; className?: string }) {
